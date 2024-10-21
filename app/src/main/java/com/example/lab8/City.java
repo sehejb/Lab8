07354101be77239966210d84de42a64d9e1f2448
Sehej Brar <sehejpur@ualberta.ca>
2024-10-21 11:48:30 -0600

    Expected to fail: no implementation for hasCity

--- app/src/main/java/com/example/lab8/City.java
+++ app/src/main/java/com/example/lab8/City.java
@@ -1,5 +1,7 @@
 package com.example.lab8;
 
+import java.util.Objects;
+
 public class City {
 
     private String city;
@@ -16,5 +18,25 @@ public class City {
 
     String getProvinceName(){
         return this.province;
+    }
+
+    @Override
+    public boolean equals(Object city) {
+        if (this == city) {
+            return true;
+        }
+
+        if (city == null || getClass() != city.getClass()) {
+            return false;
+        }
+
+        City city1 = (City) city;
+        return Objects.equals(this.city, city1.getCityName())
+                && Objects.equals(this.province, city1.getProvinceName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, province);
     }
 }
--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -58,5 +58,9 @@ public class CustomList extends ArrayAdapter<City> {
         cities.add(city);
     }
 
+    public boolean hasCity(City city) {
+        return false;
+    }
+
 
 }
